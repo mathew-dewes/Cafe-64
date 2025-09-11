db0c4d7131993469a92a3db0e31c66371db9df9a
Mathew Dewes <mathewdewes01@gmail.com>
2025-09-11 17:39:53 +1200

    Changes to auth check on dashboard routes

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,17 +1,8 @@
-import { auth } from "@/auth";
-import LoginForm from "./_components/LoginForm";
-import { headers } from "next/headers";
-import { redirect } from "next/navigation";
 
+import LoginForm from "./_components/LoginForm";
 
 
 export default async function page(){
-
-        const session = await auth.api.getSession({
-        headers: await headers()
-    });
-
-    if (session) redirect('/dashboard/analytics')
     return (
         <div>
             <h1 className="text-center lg:text-left">Admin login</h1>
