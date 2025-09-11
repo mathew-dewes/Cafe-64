aa8ecfcb53844b8d0023f601605017582fb12179
Mathew Dewes <mathewdewes01@gmail.com>
2025-09-11 14:37:21 +1200

    Code fixes for deployment

--- auth.ts
+++ auth.ts
@@ -1,13 +1,14 @@
-import { PrismaClient } from "@/app/generated/prisma";
+
+import { PrismaClient } from "@prisma/client";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-// If your Prisma file is located elsewhere, you can change the path
+
 
 
 const prisma = new PrismaClient();
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
+        provider: "postgresql", 
         
     }),
     emailAndPassword:{
